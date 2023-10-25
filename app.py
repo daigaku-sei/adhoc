578f28a90cbd7e8dd22b06268b9a6b341293ef5d
daigaku-sei <146640208+daigaku-sei@users.noreply.github.com>
2023-10-25 21:36:01 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,49 +1,4 @@
 import streamlit as st
-import json
-
-# Define the options for each configuration
-options = {
-    'Theme': ['Light', 'Dark'],
-    'Password Protection': ['Yes', 'No'],
-    'Draw Animation': ['Yes', 'No']
-    # Add more configuration options here
-}
-
-# Function to convert options to binary format
-def options_to_binary(config_options):
-    binary_string = ''
-    for option, choices in options.items():
-        selected_option = config_options[option]
-        binary_string += '1' if selected_option == choices[0] else '0'
-    return int(binary_string, 2)
-
-# Function to convert binary format to options
-def binary_to_options(binary_value):
-    binary_string = bin(binary_value)[2:].zfill(8)
-    config_options = {}
-    for i, (option, choices) in enumerate(options.items()):
-        selected_option = choices[0] if binary_string[i] == '1' else choices[1]
-        config_options[option] = selected_option
-    return config_options
-
-# Function to save the config file
-def save_config(config_name, config_options):
-    binary_value = options_to_binary(config_options)
-    with open(f'{config_name}.config', 'w') as f:
-        f.write(str(binary_value))
-    st.success(f'Config file {config_name}.config saved successfully!')
-
-# Function to load the config file
-def load_config(config_name):
-    try:
-        with open(f'{config_name}.config', 'r') as f:
-            binary_value = int(f.read())
-        config_options = binary_to_options(binary_value)
-        st.success(f'Config file {config_name}.config loaded successfully!')
-        return config_options
-    except FileNotFoundError:
-        st.error(f'Config file {config_name}.config not found!')
-        return None
 
 # Main Streamlit app
 def main():
@@ -79,29 +34,9 @@ def main():
     # Change theme dynamically based on the selected option
     if 'Theme' in config_options:
         if config_options['Theme'] == 'Dark':
-            st.markdown(
-                """
-                <style>
-                body {
-                    background-color: #1f2937;
-                    color: white;
-                }
-                </style>
-                """,
-                unsafe_allow_html=True
-            )
+            st.set_theme('dark')
         else:
-            st.markdown(
-                """
-                <style>
-                body {
-                    background-color: white;
-                    color: black;
-                }
-                </style>
-                """,
-                unsafe_allow_html=True
-            )
+            st.set_theme('light')
 
 # Run the app
 if __name__ == '__main__':
